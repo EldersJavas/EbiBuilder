0ad95f9e8f9469d339ed8c3dfd2d71ab6b9091ee
EldersJavas <55266266+EldersJavas@users.noreply.github.com>
2022-07-11 14:33:35 +0000

    feat✨:  cmd app

---
+++ app/app.go
@@ -0,0 +1,41 @@
+package app
+
+import (
+	"github.com/gookit/gcli/v3"
+	"github.com/gookit/gcli/v3/builtin"
+)
+
+type App struct{
+	CmdApp *gcli.App
+}
+
+
+
+func (a *App)Init(){
+	a.CmdApp = gcli.NewApp(func(app *gcli.App) {
+		app.Version = "3.0.0"
+		app.Desc = "this is my cli application"
+		app.On(gcli.EvtAppInit, func(data ...interface{}) bool {
+			// do something...
+			// fmt.Println("init app")
+			return false
+		})
+	
+		// app.SetVerbose(gcli.VerbDebug)
+		// app.DefaultCommand("example")
+		app.Logo.Text = ``
+	})
+	//a.CmdApp.Add()
+	a.CmdApp.Add(&gcli.Command{
+		Name: "demo",
+		// allow color tag and {$cmd} will be replace to 'demo'
+		Desc: "this is a description <info>message</> for command", 
+		Aliases: []string{"dm"},
+		Func: func (cmd *gcli.Command, args []string) error {
+			gcli.Println("hello, in the demo command")
+			return nil
+		},
+	})
+	a.CmdApp.Add(builtin.GenAutoComplete())
+	// .... add more ...
+	}
--- main.go
+++ main.go
@@ -2,39 +2,13 @@ package main
 
 import (
 	//"runtime"
-	"github.com/gookit/gcli/v3"
-	"github.com/gookit/gcli/v3/_examples/cmd"
-	"github.com/gookit/gcli/v3/builtin"
+	"github.com/EldersJavas/EbiBuilder/app"
 )
+
+var App app.App
 
 // 测试运行: go run ./_examples/cliapp.go && ./cliapp
 func main() {
-    app := gcli.NewApp(func(app *gcli.App) {
-		app.Version = "3.0.0"
-		app.Desc = "this is my cli application"
-		app.On(gcli.EvtAppInit, func(data ...interface{}) bool {
-			// do something...
-			// fmt.Println("init app")
-			return false
-		})
-
-		// app.SetVerbose(gcli.VerbDebug)
-		// app.DefaultCommand("example")
-		app.Logo.Text = ``
-	})
-    app.Add(cmd.Example)
-    app.Add(&gcli.Command{
-        Name: "demo",
-        // allow color tag and {$cmd} will be replace to 'demo'
-        Desc: "this is a description <info>message</> for command", 
-        Aliases: []string{"dm"},
-        Func: func (cmd *gcli.Command, args []string) error {
-            gcli.Println("hello, in the demo command")
-            return nil
-        },
-    })
-	app.Add(builtin.GenAutoComplete())
-    // .... add more ...
-
-    app.Run(nil)
+    App.Init()
+	App.CmdApp.Run(nil)
 }
